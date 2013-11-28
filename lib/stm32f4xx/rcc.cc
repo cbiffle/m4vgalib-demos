1e92435e66388f091a32b717ee46c1ef07bfaa8e
Cliff L. Biffle <code@cliffle.com>
2013-11-28 15:37:21 -0800

    stm32f4xx/rcc: AHB clock/reset control.

--- lib/stm32f4xx/rcc.cc
+++ lib/stm32f4xx/rcc.cc
@@ -47,4 +47,53 @@ void Rcc::disable_clock(ApbPeripheral p) {
   armv7m::data_synchronization_barrier();
 }
 
+
+void Rcc::enter_reset(AhbPeripheral p) {
+  unsigned bus = get_bus_index(p);
+  unsigned slot = get_reset_index(p);
+
+  switch (bus) {
+    case 0: write_ahb1rstr(read_ahb1rstr().with_bit(slot, true)); break;
+    case 1: write_ahb2rstr(read_ahb2rstr().with_bit(slot, true)); break;
+    case 2: write_ahb3rstr(read_ahb3rstr().with_bit(slot, true)); break;
+  }
+  armv7m::data_synchronization_barrier();
+}
+
+void Rcc::leave_reset(AhbPeripheral p) {
+  unsigned bus = get_bus_index(p);
+  unsigned slot = get_reset_index(p);
+
+  switch (bus) {
+    case 0: write_ahb1rstr(read_ahb1rstr().with_bit(slot, false)); break;
+    case 1: write_ahb2rstr(read_ahb2rstr().with_bit(slot, false)); break;
+    case 2: write_ahb3rstr(read_ahb3rstr().with_bit(slot, false)); break;
+  }
+  armv7m::data_synchronization_barrier();
+}
+
+void Rcc::enable_clock(AhbPeripheral p) {
+  unsigned bus = get_bus_index(p);
+  unsigned slot = get_enable_index(p);
+
+  switch (bus) {
+    case 0: write_ahb1enr(read_ahb1enr().with_bit(slot, true)); break;
+    case 1: write_ahb2enr(read_ahb2enr().with_bit(slot, true)); break;
+    case 2: write_ahb3enr(read_ahb3enr().with_bit(slot, true)); break;
+  }
+  armv7m::data_synchronization_barrier();
+}
+
+void Rcc::disable_clock(AhbPeripheral p) {
+  unsigned bus = get_bus_index(p);
+  unsigned slot = get_enable_index(p);
+
+  switch (bus) {
+    case 0: write_ahb1enr(read_ahb1enr().with_bit(slot, false)); break;
+    case 1: write_ahb2enr(read_ahb2enr().with_bit(slot, false)); break;
+    case 2: write_ahb3enr(read_ahb3enr().with_bit(slot, false)); break;
+  }
+  armv7m::data_synchronization_barrier();
+}
+
 }  // namespace stm32f4xx
