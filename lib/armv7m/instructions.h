f1401f1ecd7c4e00cbf342a9edc218c797a2f792
Cliff L. Biffle <code@cliffle.com>
2013-12-08 11:05:35 -0800

    lib/armv7m: add usat.

--- lib/armv7m/instructions.h
+++ lib/armv7m/instructions.h
@@ -55,6 +55,28 @@ INLINE void instruction_synchronization_barrier() {
   asm volatile ("isb");
 }
 
+/*
+ * Saturate an unsigned integer to a certain number of bit positions with an
+ * optional shift.  Negative shift distance shift right.
+ */
+template <unsigned N, int S = 0>
+unsigned usat(unsigned v) {
+  register unsigned r;
+  if (S >= 0) {
+    static_assert(S < 0 || (S <= 31), "left shift out of range");
+    asm ("usat %0, %1, %2, LSL %3"
+         : "=r" (r)
+         : "I" (N), "r" (v), "I" (S));
+    return r;
+  } else {
+    static_assert(S >= 0 || (-S >= 1 && -S <= 31), "right shift out of range");
+    asm ("usat %0, %1, %2, ASR %3"
+         : "=r" (r)
+         : "I" (N), "r" (v), "I" (-S));
+    return r;
+  }
+}
+
 }  // namespace armv7m
 
 #endif  // LIB_ARMV7M_INSTRUCTIONS_H
