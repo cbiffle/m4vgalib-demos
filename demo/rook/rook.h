dcea115cf50e98dc141f0b4f6b1a7a6d03bd3c07
Cliff L. Biffle <code@cliffle.com>
2013-12-13 17:06:00 -0800

    demo/rook: factor demo out of main.

---
+++ demo/rook/rook.h
@@ -0,0 +1,12 @@
+#ifndef DEMO_ROOK_ROOK_H
+#define DEMO_ROOK_ROOK_H
+
+namespace demo {
+namespace rook {
+
+void run(unsigned frame_count);
+
+}  // namespace rook
+}  // namespace demo
+
+#endif  // DEMO_ROOK_ROOK_H
