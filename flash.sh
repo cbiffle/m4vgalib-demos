26b33305992a9fbf6dd255604914a1ee53b2d941
Cliff L. Biffle <code@cliffle.com>
2014-05-11 11:51:25 -0700

    Add flash.sh, update README to describe it.

---
+++ flash.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+# Intended use:
+#   $ ./flash.sh build/latest/demo/rook/demo
+# The openocd.cfg is specific to the STLink/V2 programmer, like the one found
+# on the STM32F4DISCOVERY board.
+#
+# Note that openocd support for this programmer is a little flaky at times, and
+# if you need to flash repeatedly, you'll have better luck running a single
+# continuous openocd session and using gdb.sh.
+
+openocd -f openocd.cfg \
+  -c "init" \
+  -c "reset init"
+  -c "flash write_image erase $1" \
+  -c "reset halt"
+  -c "resume"
+  -c "shutdown"
--- gdb.sh
+++ gdb.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# Intended use:
+#   $ gdb.sh build/latest/demo/rook/demo
+# Note that you must have started openocd separately for this to work.
+# Once inside gdb, the gdbconfig gives you a 'flash' command.
+
 arm-none-eabi-gdb -x gdbconfig $1
